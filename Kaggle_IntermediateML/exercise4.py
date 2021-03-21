3f4c528183c3771575366a6e0aab53583c001c79
Jonghyun Choe <andy.choe729@gmail.com>
2021-03-21 21:26:35 +0900

    3/21/21 Study

--- Kaggle_IntermediateML/exercise4.py
+++ Kaggle_IntermediateML/exercise4.py
@@ -69,3 +69,42 @@ clf.fit(X_train, y_train)
 preds = clf.predict(X_valid) 
 
 print('MAE:', mean_absolute_error(y_valid, preds))
+
+# Preprocessing for numerical data 
+numerical_transformer = SimpleImputer() 
+
+# Preprocessing for categorical data 
+categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+
+# Bundle preprocessing for numerical and categorical data 
+preprocessor = ColumnTransformer(
+    transformers = [
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ]
+)
+
+# Define model 
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Bundle preprocessing and modeling code in a pipeline 
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor), 
+                              ('model', model)
+                        ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train) 
+
+# Preprocessing of validation data, get predictions 
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model 
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
+
+# Preprocessing of test data, fit model 
+preds_test = my_pipeline.predict(X_test) 
+
+# Save test prediction to file
+output = pd.DataFrame({'Id': X_test.index, 'SalePrice': preds_test})
+output.to_csv('submission.csv', index=False)
